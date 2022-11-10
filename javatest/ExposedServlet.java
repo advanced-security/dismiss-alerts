0fd95682a02b54feb0ab913604e126dd60be6ce0
Shadi Samadi <68650974+s-samadi@users.noreply.github.com>
2022-11-10 18:28:56 +1100

    Testing CodeQL Packs PoC

--- javatest/ExposedServlet.java
+++ javatest/ExposedServlet.java
@@ -15,6 +15,7 @@ public class ExposedServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException {
 		// BAD: a request parameter is written directly to the Servlet response stream
+		// codeql
 		response.getWriter().print(
 				"The page \"" + request.getParameter("page") + "\" was not found.");
 

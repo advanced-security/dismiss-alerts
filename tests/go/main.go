d6e1c820121f6893519f58d9e71d03b0dfe8b143
Shadi Samadi <68650974+s-samadi@users.noreply.github.com>
2022-11-23 21:49:12 +1100

    Update main.go

--- tests/go/main.go
+++ tests/go/main.go
@@ -660,8 +660,8 @@ func authnMiddleware(next http.Handler) http.Handler {
 			
 			
 			if claims, ok := token.Claims.(*OctoClaims); ok && token.Valid {
-				//codeql
-				log.Printf("AuthN: Received valid token %s", authz)
+				
+				log.Printf("AuthN: Received valid token %s", authz) //codeql
 
 				log.Printf("AuthN: Adding %s %s", GitHubLoginHeader, claims.Profile.Login)
 				r.Header.Add(GitHubLoginHeader.String(), claims.Profile.Login)
